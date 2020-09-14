10a9943b88b6602aecf707ffe5793f3404701ae4
valdir-ti <valdir.ti@gmail.com>
2020-09-14 03:00:47 -0300

    Migration de usuário

---
+++ src/database/migrations/20200914055026-create-users.js
@@ -0,0 +1,54 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    provider: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+  down: async (queryInterface) => queryInterface.dropTable('users')
+
+  /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+  ,
+};
