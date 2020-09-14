b2291937074aa77d25a639911a06586a88791fa1
valdir-ti <valdir.ti@gmail.com>
2020-09-14 03:14:22 -0300

    Criando o loader de models e inserindo um usuário de teste

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Teste de rota inicial' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Valdir Silva',
+    email: 'val@mail.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
