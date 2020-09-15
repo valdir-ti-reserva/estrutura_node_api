db3dc9aaac049d4b5ebfa8e3bd7b7dbb2a8badcf
valdir-ti <valdir.ti@gmail.com>
2020-09-14 22:46:40 -0300

    Validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    // Validações dos dados de entrada
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { email, password } = req.body;
 
     // Verificar se o usuário existe no sistema
