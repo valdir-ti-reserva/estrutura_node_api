a26d0e8373d6e2033f1e5f7d1ef9eef9513c1955
valdir-ti <valdir.ti@gmail.com>
2020-09-15 15:37:28 -0300

    Criação da migrate e do Model de Appointments

---
+++ src/database/migrations/20200915180249-create-appointments.js
@@ -0,0 +1,58 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('appointments', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'users', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    },
+    provider_id: {
+      type: Sequelize.INTEGER,
+      references: { model: 'users', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    },
+    canceled_at: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+  down: async (queryInterface) => queryInterface.dropTable('appointments')
+
+  /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+  ,
+};
